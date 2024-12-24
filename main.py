62be75b60b83a198f35c01733fcdbb0023ff16aa
koyaka <sasha.kolyaka@gmail.com>
2024-12-24 16:14:01 +0300

    Синхронизация

--- main.py
+++ main.py
@@ -1,61 +1,72 @@
 import cv2
 import datetime
 
+# Задаем параметры видео
+width, height = 640, 480  # Размеры видео
+fps = 30  # Частота кадров
+output_file = 'data/result.mp4'  # Имя выходного файла
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Кодек для AVI
+out = cv2.VideoWriter(output_file, fourcc, fps, (width * 2, height * 2))
 
 def read_annotations(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
+def show_result(annotation, index_annotation):
+    annotation_float = float(annotation[index_annotation])
+    annotation_date = datetime.datetime.fromtimestamp(annotation_float)
+    complete_date = str(annotation_date.strftime('%H:%M:%S:%f'))
+    return complete_date
 
+
+# Чтение аннотаций
 first_annotations = read_annotations('data/1.txt')
 second_annotations = read_annotations('data/2.txt')
 third_annotations = read_annotations('data/3.txt')
 fourth_annotations = read_annotations('data/4.txt')
 
+# Открытие видео
 first_video = cv2.VideoCapture("data/1.avi")
 second_video = cv2.VideoCapture("data/2.avi")
 third_video = cv2.VideoCapture("data/3.avi")
 fourth_video = cv2.VideoCapture("data/4.avi")
 
-frame_index1 = 0
-frame_index2 = 0
-frame_index3 = 0
-frame_index4 = 0
+frame_index1 = frame_index2 = frame_index3 = frame_index4 = 0
 
 fps = 5
 delay = int(1000 / fps)
 
-# Хранение предыдущих кадров
-prev_frame1 = None
-prev_frame2 = None
-prev_frame3 = None
-prev_frame4 = None
+prev_frame1 = prev_frame2 = prev_frame3 = prev_frame4 = None
+ret1, frame1 = first_video.read()
+ret2, frame2 = second_video.read()
+ret3, frame3 = third_video.read()
+ret4, frame4 = fourth_video.read()
 
 while True:
-    ret1, frame1 = first_video.read()
-    ret2, frame2 = second_video.read()
-    ret3, frame3 = third_video.read()
-    ret4, frame4 = fourth_video.read()
+            # Получаем временные метки
+    timestamp_sec1 = float(first_annotations[frame_index1])
+    timestamp_sec2 = float(second_annotations[frame_index2])
+    timestamp_sec3 = float(third_annotations[frame_index3])
+    timestamp_sec4 = float(fourth_annotations[frame_index4])
 
-    # Изменение размера кадров для удобства отображения
+
+    # Если все видео закончились, выходим из цикла
+    if not (ret1 or ret2 or ret3 or ret4):
+        break
+
+    # Обработка кадров
     if ret1:
         frame1 = cv2.resize(frame1, (640, 480))
         if frame_index1 < len(first_annotations):
-            annotation_float = float(first_annotations[frame_index1])
-            annotation_date = datetime.datetime.fromtimestamp(annotation_float / 1000)
-            complete_date = str(annotation_date.strftime('%H:%M:%S.%f'))
-            cv2.putText(frame1, complete_date, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        prev_frame1 = frame1  # Сохраняем текущий кадр как предыдущий
+            cv2.putText(frame1, f'1: {show_result(first_annotations, frame_index1)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        prev_frame1 = frame1
     else:
-        frame1 = prev_frame1  # Используем предыдущий кадр, если текущий не доступен
+        frame1 = prev_frame1
 
     if ret2:
         frame2 = cv2.resize(frame2, (640, 480))
         if frame_index2 < len(second_annotations):
-            annotation_float = float(second_annotations[frame_index2])
-            annotation_date = datetime.datetime.fromtimestamp(annotation_float / 1000)
-            complete_date = str(annotation_date.strftime('%H:%M:%S.%f'))
-            cv2.putText(frame1, complete_date, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame1, f'2: {show_result(second_annotations, frame_index2)}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         prev_frame2 = frame2
     else:
         frame2 = prev_frame2
@@ -63,10 +74,7 @@ while True:
     if ret3:
         frame3 = cv2.resize(frame3, (640, 480))
         if frame_index3 < len(third_annotations):
-            annotation_float = float(third_annotations[frame_index3])
-            annotation_date = datetime.datetime.fromtimestamp(annotation_float / 1000)
-            complete_date = str(annotation_date.strftime('%H:%M:%S.%f'))
-            cv2.putText(frame1, complete_date, (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame1, f'3: {show_result(third_annotations, frame_index3)}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         prev_frame3 = frame3
     else:
         frame3 = prev_frame3
@@ -74,34 +82,82 @@ while True:
     if ret4:
         frame4 = cv2.resize(frame4, (640, 480))
         if frame_index4 < len(fourth_annotations):
-            annotation_float = float(fourth_annotations[frame_index4])
-            annotation_date = datetime.datetime.fromtimestamp(annotation_float / 1000)
-            complete_date = str(annotation_date.strftime('%H:%M:%S.%f'))
-            cv2.putText(frame1, complete_date, (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame1, f'4: {show_result(fourth_annotations, frame_index4)}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         prev_frame4 = frame4
     else:
         frame4 = prev_frame4
 
-    # Объединение кадров в одно изображение
-    top_row = cv2.hconcat([frame1, frame2])
-    bottom_row = cv2.hconcat([frame3, frame4])
-    combined_frame = cv2.vconcat([top_row, bottom_row])
+    # Проверка на синхронизацию с небольшим отклонением
+    timestamps = [timestamp_sec1, timestamp_sec2, timestamp_sec3, timestamp_sec4]
+    min_timestamp = min(timestamps)
+    max_timestamp = max(timestamps)
+    max_index = timestamps.index(max_timestamp)
+    result = max_timestamp - min_timestamp
 
-    # Отображение объединенного кадра
-    cv2.imshow('Synchronized Video Playback', combined_frame)
+    if result >= 0.3:
+        # Объединение кадров в одно изображение
+        if max_index == 0:
+            frame1 = prev_frame1
+            frame_index2 += 1
+            frame_index3 += 1
+            frame_index4 += 1
+        elif max_index == 1:
+            frame2 = prev_frame2
+            frame_index1 += 1
+            frame_index3 += 1
+            frame_index4 += 1
+        elif max_index == 2:
+            frame3 = prev_frame3
+            frame_index2 += 1
+            frame_index1 += 1
+            frame_index4 += 1
+        else:
+            frame4 = prev_frame4
+            frame_index2 += 1
+            frame_index3 += 1
+            frame_index1 += 1
+        top_row = cv2.hconcat([frame1, frame2])
+        bottom_row = cv2.hconcat([frame3, frame4])
+        combined_frame = cv2.vconcat([top_row, bottom_row])
 
-    # Увеличиваем счетчики кадров, если текущие кадры доступны
-    if ret1 and frame_index1 < len(first_annotations):
+        # Записываем кадр в видео
+        out.write(combined_frame)
+
+        # Отображение объединенного кадра
+        cv2.imshow('Synchronized Video Playback', combined_frame)
+        # Объединение кадров в одно изображение
+        if max_index == 0:
+            ret2, frame2 = second_video.read()
+            ret3, frame3 = third_video.read()
+            ret4, frame4 = fourth_video.read()
+        elif max_index == 1:
+            ret1, frame1 = first_video.read()
+            ret3, frame3 = third_video.read()
+            ret4, frame4 = fourth_video.read()
+        elif max_index == 2:
+            ret1, frame1 = first_video.read()
+            ret2, frame2 = second_video.read()
+            ret4, frame4 = fourth_video.read()
+        else:
+            ret1, frame1 = first_video.read()
+            ret2, frame2 = second_video.read()
+            ret3, frame3 = third_video.read()
+    else:
         frame_index1 += 1
-    if ret2 and frame_index2 < len(second_annotations):
         frame_index2 += 1
-    if ret3 and frame_index3 < len(third_annotations):
         frame_index3 += 1
-    if ret4 and frame_index4 < len(fourth_annotations):
         frame_index4 += 1
-
+        # Если видео не синхронизированы, показываем текущие кадры
+        current_frame = cv2.hconcat([frame1, frame2])
+        current_frame = cv2.vconcat([current_frame, cv2.hconcat([frame3, frame4])])
+        cv2.imshow('Synchronized Video Playback', current_frame)
+        ret1, frame1 = first_video.read()
+        ret2, frame2 = second_video.read()
+        ret3, frame3 = third_video.read()
+        ret4, frame4 = fourth_video.read()
+        
     # Выход при нажатии клавиши 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(100) & 0xFF == ord('q'):
         break
 
 # Освобождение ресурсов
@@ -109,4 +165,5 @@ first_video.release()
 second_video.release()
 third_video.release()
 fourth_video.release()
+out.release()
 cv2.destroyAllWindows()
